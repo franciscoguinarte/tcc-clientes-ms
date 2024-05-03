910132dc263bb0fc6829aea0e0b97b5673475511
franciscoguinarte <francisco.guinarte@outlook.com.br>
2024-05-03 11:13:31 -0300

    corrigindo model

--- src/main/kotlin/br/com/francisco/model/Cliente.kt
+++ src/main/kotlin/br/com/francisco/model/Cliente.kt
@@ -12,35 +12,12 @@ data class Cliente (
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     val id : Long,
-
-    @NotEmpty
-    @RegExp(prefix = "^\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}\$\n")
     val cpf : String,
-
-    @NotNull
-    @NotBlank
     val nome : String,
-
-    @NotNull
     val endereco : String,
-
-    @NotBlank
-    @RegExp(prefix = "^\\(\\d{2}\\)\\s?\\d{4,5}-\\d{4}\$\n")
     val contato: String,
-
-    @NotBlank
-    @NotNull
     val placaVeiculo : String,
-
-    @NotBlank
-    @NotNull
     val modeloVeiculo: String,
-
-    @NotBlank
-    @NotNull
     val valorVeiculo : BigDecimal,
-
-    @NotBlank
-    @NotNull
     val anoVeiculo : Long
 )
